91833103a678bbe8290424cdf531dff4673a7078
Pascal Huerst <pascal.huerst@gmail.com>
2018-04-10 15:59:42 +0200

    Revert "Revert "fring-protocol: Fixed attribute-packed in structs and refactored""
    This reverts commit 87735d047aa5e35f944ebe898a41db93066ba2a3.

--- fring-protocol.h
+++ fring-protocol.h
@@ -1,6 +1,8 @@
 #ifndef FRINGPROTOCOL_H
 #define FRINGPROTOCOL_H
 
+namespace FringProtocol {
+
 enum {
     FRING_REG_ID                          = 0x01,
     FRING_REG_READ_BOOT_INFO              = 0x02,
@@ -63,117 +65,140 @@ enum {
 
 #define _packed_ __attribute__((__packed__))
 
-struct FringCommandRead {
+struct Id {
+    uint8_t id[5];
+} _packed_;
+
+struct BootInfo {
+    uint32_t version;
+    uint32_t uptime;
+    uint32_t flags;
+    uint8_t serial[12];
+} _packed_;
+
+struct BoardRevision {
+    uint8_t hardwareType;
+    uint8_t boardRevisionA;
+    uint8_t boardRevisionB;
+    uint8_t dummy;
+} _packed_;
+
+struct InterruptStatus {
+    uint32_t status;
+} _packed_;
+
+struct DeviceStatus {
+    uint32_t status;
+    uint32_t hardwareErrors;
+    uint8_t ambientLightValue;
+    uint8_t temp0;
+    uint8_t temp1;
+    uint8_t temp2;
+} _packed_;
+
+struct BatteryStatus {
+    int8_t chargeCurrent;
+    uint8_t level;
+    uint8_t temp;
+    uint8_t dummy;
+    uint16_t status;
+    uint16_t remainingCapacity;
+    uint16_t averageTimeToEmpty;
+    uint16_t averageTimeToFull;
+    uint16_t cycleCount;
+} _packed_;
+
+struct UpdateStatus {
+    uint32_t status;
+} _packed_;
+
+struct WakeupReason {
+    uint32_t reason;
+} _packed_;
+
+struct CommandRead {
     union {
-        struct {
-            uint8_t id[5];
-        } id;
-
-        struct {
-            uint32_t version;
-            uint32_t uptime;
-            uint8_t serial[12];
-            uint32_t flags;
-        } bootInfo _packed_;
-
-        struct {
-            uint8_t hardwareType;
-            uint8_t boardRevisionA;
-            uint8_t boardRevisionB;
-        } boardRevision;
-
-        struct {
-            uint32_t status;
-        } interruptStatus;
-
-        struct {
-            uint8_t ambientLightValue;
-            uint32_t status;
-            uint32_t hardwareErrors;
-            uint8_t temp0;
-            uint8_t temp1;
-            uint8_t temp2;
-        } deviceStatus;
-
-        struct {
-            int8_t chargeCurrent;
-            uint8_t level;
-            uint8_t temp;
-            uint16_t status;
-            uint16_t remainingCapacity;
-            uint16_t averageTimeToEmpty;
-            uint16_t averageTimeToFull;
-            uint16_t cycleCount;
-        } batteryStatus;
-
-        struct {
-            uint32_t status;
-        } updateStatus;
-
-        struct {
-            uint32_t reason;
-        } wakeupReason;
-
-        uint8_t unused[0];
-    };
-}  _packed_;
-
-struct FringCommandWrite {
-    uint8_t reg;
-    union {
-        struct {
-            uint8_t version;
-        } protocolVersion;
-
-        struct {
-            uint8_t id;
-            uint8_t mode;
-
-            union {
-                struct {
-                } off;
-
-                struct {
-                    uint8_t r;
-                    uint8_t g;
-                    uint8_t b;
-                } on;
-
-                struct {
-                    uint8_t r;
-                    uint8_t g;
-                    uint8_t b;
-                    uint8_t on;
-                    uint8_t off;
-                } flashing;
-
-                struct {
-                    uint8_t r;
-                    uint8_t g;
-                    uint8_t b;
-                    uint8_t period;
-                } pulsating;
-            };
-        } led;
-
-        struct {
-            uint32_t length;
-            uint32_t offset;
-            uint32_t crc;
-            char payload[0];
-        } firmwareUpdate;
-
-        struct {
-            uint32_t seconds;
-        } wakeupTime;
-
-        struct {
-            uint8_t serial[12];
-        } setSerial;
+        Id id;
+        BootInfo bootInfo;
+        BoardRevision boardRevision;
+        InterruptStatus interruptStatus;
+        DeviceStatus deviceStatus;
+        BatteryStatus batteryStatus;
+        UpdateStatus updateStatus;
+        WakeupReason wakeupReason;
 
         uint8_t unused[0];
     };
 } _packed_;
 
+struct Protocol {
+    uint8_t version;
+} _packed_;
+
+struct Off {
+} _packed_;
+
+struct On {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+} _packed_;
+
+struct Flashing {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t on;
+    uint8_t off;
+} _packed_;
+
+struct Pulsating {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t period;
+} _packed_;
+
+struct Led {
+    uint8_t id;
+    uint8_t mode;
+
+    union {
+        Off off;
+        On on;
+        Flashing flashing;
+        Pulsating pulsating;
+    };
+} _packed_;
+
+struct FirmwareUpdate {
+    uint32_t length;
+    uint32_t offset;
+    uint32_t crc;
+    char payload[0];
+} _packed_;
+
+struct WakeupTime {
+    uint32_t seconds;
+} _packed_;
+
+struct Serial {
+    uint8_t serial[12];
+} _packed_;
+
+struct CommandWrite {
+    uint8_t reg;
+    union {
+        Protocol protocol;
+        Led led;
+        FirmwareUpdate firmwareUpdate;
+        WakeupTime wakeupTime;
+        Serial serial;
+
+        uint8_t unused[0];
+    };
+} _packed_;
+
+} // namepsace FringProtocol
 
 #endif // FRINGPROTOCOL_H
